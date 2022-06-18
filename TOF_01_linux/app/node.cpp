a6abec6e751d24c66a5364c051fc6025cd97e46b
Carlos  chen <ccjjww1222@gmail.com>
2022-06-18 11:36:40 +0800

    2
    3

--- TOF_01_linux/app/node.cpp
+++ TOF_01_linux/app/node.cpp
@@ -83,10 +83,10 @@ int main(int argc, char * argv[])
                     send_lidar_scan_data[i] = one_lidar_data;
                 }
                 
-                int size = 30;
+                int currentSize = 30;
                 // printf("Lidar count %d!\n", (int)lidar_scan_size);
                 // printf("Lidar  size:%d, lidar_scan_size:%d,distance:%f,angle:%f,signal:%f!\n",(int)sizeof(send_lidar_scan_data),(int)lidar_scan_size, (float)send_lidar_scan_data[0].distance, (float)send_lidar_scan_data[0].angle, (float)send_lidar_scan_data[0].signal);
-                printf("Lidar  size:%d, lidar_scan_size:%d,distance:%f,angle:%f,signal:%f!\n",(int)sizeof(send_lidar_scan_data),(int)lidar_scan_size, (float)send_lidar_scan_data[size].distance, (float)send_lidar_scan_data[size].angle, (float)send_lidar_scan_data[size].signal);
+                printf("Lidar  size:%d,currentSize:%d, lidar_scan_size:%d,distance:%f,angle:%f,signal:%f!\n",(int)sizeof(send_lidar_scan_data),currentSize,(int)lidar_scan_size, (float)send_lidar_scan_data[currentSize].distance, (float)send_lidar_scan_data[currentSize].angle, (float)send_lidar_scan_data[currentSize].signal);
 
 
                 break;
