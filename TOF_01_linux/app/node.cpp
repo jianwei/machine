081a0d3b1acb56a9026d21cce4da66bc2b49a3b8
Carlos  chen <ccjjww1222@gmail.com>
2022-06-19 12:48:57 +0800

    2
    2

--- TOF_01_linux/app/node.cpp
+++ TOF_01_linux/app/node.cpp
@@ -1,20 +1,18 @@
 /*
-*  3iRoboticsLIDAR System II
-*  Driver Interface
-*
-*  Copyright 2017 3iRobotics
-*  All rights reserved.
-*
-*	Author: 3iRobotics, Data:2017-09-15
-*
-*/
-
+ *  3iRoboticsLIDAR System II
+ *  Driver Interface
+ *
+ *  Copyright 2017 3iRobotics
+ *  All rights reserved.
+ *
+ *	Author: 3iRobotics, Data:2017-09-15
+ *
+ */
 
 #include "C3iroboticsLidar.h"
 #include "CSerialConnection.h"
 
-#define DEG2RAD(x) ((x)*M_PI/180.)
-
+#define DEG2RAD(x) ((x)*M_PI / 180.)
 
 typedef struct _rslidar_data
 {
@@ -25,17 +23,16 @@ typedef struct _rslidar_data
         distance = 0.0;
     }
     uint8_t signal;
-    float   angle;
-    float   distance;
-}RslidarDataComplete;
+    float angle;
+    float distance;
+} RslidarDataComplete;
 
 using namespace std;
 using namespace everest::hwdrivers;
 
-
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
-	int    opt_com_baudrate = 230400;//230400;
+    int opt_com_baudrate = 230400; // 230400;
     string opt_com_path = "/dev/ttyUSB0";
 
     CSerialConnection serial_connect;
@@ -43,65 +40,62 @@ int main(int argc, char * argv[])
 
     serial_connect.setBaud(opt_com_baudrate);
     serial_connect.setPort(opt_com_path.c_str());
-    if(serial_connect.openSimple())
+    if (serial_connect.openSimple())
     {
         printf("[AuxCtrl] Open serail port sucessful!\n");
-        printf("baud rate:%d\n",serial_connect.getBaud());
+        printf("baud rate:%d\n", serial_connect.getBaud());
     }
     else
     {
         printf("[AuxCtrl] Open serail port %s failed! \n", opt_com_path.c_str());
         return -1;
-    } 
+    }
 
     printf("C3iroboticslidar connected\n");
 
     robotics_lidar.initilize(&serial_connect);
 
-
     while (1)
     {
-		TLidarGrabResult result = robotics_lidar.getScanData();
-        switch(result)
+        TLidarGrabResult result = robotics_lidar.getScanData();
+        switch (result)
         {
-            case LIDAR_GRAB_ING:
-            {
-                break;
-            }
-            case LIDAR_GRAB_SUCESS:
-            {
-                TLidarScan lidar_scan = robotics_lidar.getLidarScan();
-                size_t lidar_scan_size = lidar_scan.getSize();
-                std::vector<RslidarDataComplete> send_lidar_scan_data;
-                send_lidar_scan_data.resize(lidar_scan_size);
-                RslidarDataComplete one_lidar_data;
-                for(size_t i = 0; i < lidar_scan_size; i++)
-                {
-                    one_lidar_data.signal = lidar_scan.signal[i];
-                    one_lidar_data.angle = lidar_scan.angle[i];
-                    one_lidar_data.distance = lidar_scan.distance[i];
-                    send_lidar_scan_data[i] = one_lidar_data;
-                }
-                
-                int currentSize = 30;
-                // printf("Lidar count %d!\n", (int)lidar_scan_size);
-                // printf("Lidar  size:%d, lidar_scan_size:%d,distance:%f,angle:%f,signal:%f!\n",(int)sizeof(send_lidar_scan_data),(int)lidar_scan_size, (float)send_lidar_scan_data[0].distance, (float)send_lidar_scan_data[0].angle, (float)send_lidar_scan_data[0].signal);
-                printf("Lidar  size:%d,currentSize:%d,begin:%d,end:%d, lidar_scan_size:%d,distance:%f \n",(int)sizeof(send_lidar_scan_data),currentSize,begin(send_lidar_scan_data),end(send_lidar_scan_data),(int)lidar_scan_size, (float)send_lidar_scan_data[currentSize].distance);
-
-
-                break;
-            }
-            case LIDAR_GRAB_ERRO:
-            {
-                break;
-            }
-            case LIDAR_GRAB_ELSE:
-            {
-                printf("[Main] LIDAR_GRAB_ELSE!\n");
-                break;
-            }
+        case LIDAR_GRAB_ING:
+        {
+            break;
         }
-        //usleep(50);
+        case LIDAR_GRAB_SUCESS:
+        {
+            TLidarScan lidar_scan = robotics_lidar.getLidarScan();
+            size_t lidar_scan_size = lidar_scan.getSize();
+            std::vector<RslidarDataComplete> send_lidar_scan_data;
+            send_lidar_scan_data.resize(lidar_scan_size);
+            RslidarDataComplete one_lidar_data;
+            for (size_t i = 0; i < lidar_scan_size; i++)
+            {
+                one_lidar_data.signal = lidar_scan.signal[i];
+                one_lidar_data.angle = lidar_scan.angle[i];
+                one_lidar_data.distance = lidar_scan.distance[i];
+                send_lidar_scan_data[i] = one_lidar_data;
+            }
+            int length = sizeof(send_lidar_scan_data) / sizeof(send_lidar_scan_data[0]);
+            int currentSize = 30;
+            // printf("Lidar count %d!\n", (int)lidar_scan_size);
+            // printf("Lidar  size:%d, lidar_scan_size:%d,distance:%f,angle:%f,signal:%f!\n",(int)sizeof(send_lidar_scan_data),(int)lidar_scan_size, (float)send_lidar_scan_data[0].distance, (float)send_lidar_scan_data[0].angle, (float)send_lidar_scan_data[0].signal);
+            printf("Lidar  size:%d,currentSize:%d, lidar_scan_size:%d,distance:%f \n", (int)length, currentSize, (int)lidar_scan_size, (float)send_lidar_scan_data[currentSize].distance);
+            break;
+        }
+        case LIDAR_GRAB_ERRO:
+        {
+            break;
+        }
+        case LIDAR_GRAB_ELSE:
+        {
+            printf("[Main] LIDAR_GRAB_ELSE!\n");
+            break;
+        }
+        }
+        // usleep(50);
     }
 
     return 0;
