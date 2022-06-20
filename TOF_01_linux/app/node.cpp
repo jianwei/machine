04628c75282a45e7219b0ae76cf0e54502aa8428
Carlos  chen <ccjjww1222@gmail.com>
2022-06-20 22:00:05 +0800

    xbox
    xbox

--- TOF_01_linux/app/node.cpp
+++ TOF_01_linux/app/node.cpp
@@ -80,7 +80,7 @@ int main(int argc, char *argv[])
                 send_lidar_scan_data[i] = one_lidar_data;
                 if (one_lidar_data.distance > 0)
                 {
-                    printf("i=%d/%d,angle:%f,distance:%f \n", (int)i,(int)lidar_scan_size, (float)one_lidar_data.angle, (float)one_lidar_data.distance);
+                    printf("i=%d/%d,angle:%f,distance:%f \n", (int)i, (int)lidar_scan_size, (float)one_lidar_data.angle, (float)one_lidar_data.distance);
                 }
             }
             // int length = sizeof(send_lidar_scan_data) / sizeof(send_lidar_scan_data[0]);
