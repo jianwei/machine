e159190ac82d56931833e0026ca50b4223927d0a
Carlos  chen <ccjjww1222@gmail.com>
2022-07-06 22:57:34 +0800

    distance
    distance

--- yolov5/distance.py
+++ yolov5/distance.py
@@ -1,16 +1,33 @@
+
+v = 950
 def imgDistance(points):
     h = 950 #高度 cm
-    distance = convert(h,points)
+    distance = convert(points)
     return distance
 
 # v 物距   雷达扫描 单位mm
-def convert(v,points):
+def convert(points):
+    global v
     unit  = 1.4 #1.4um  像素大小
     f = 3.6  # 3.6mm 焦距
     ratio = v/f
+    s= 4 #调整系数
     realPoints  = []
     for point in points:
-        realPoints.append((point[0]*unit*ratio/1000/10,point[1]*unit*ratio/1000/10)) 
+        realPoints.append((point[0]*unit*ratio/1000/10*s,point[1]*unit*ratio/1000/10*s)) 
     # print ("实际距离，单位厘米:",realPoints)
-    return realPoints
     
+    return realPoints
+
+def getYDistance (points):
+    global v
+    center  = getCenter(points)
+    pass
+
+
+def getCenter(points):
+    xCenter = (points[1][0] - points[0][0])/2
+    yCenter = (points[2][1] - points[0][1])/2
+    center = (xCenter,yCenter)
+    # print ("center:",center)
+    return center
