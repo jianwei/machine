fcf6f3e83e3413b2e4f58317c87cb83c989a56db
Carlos  chen <ccjjww1222@gmail.com>
2022-07-15 22:58:40 +0800

    2
    3

--- src/weeding/weeding/utils/motor.py
+++ src/weeding/weeding/utils/motor.py
@@ -115,3 +115,33 @@ if __name__ == '__main__':  # Program start from here
         loop()
    except KeyboardInterrupt:  # When 'Ctrl+C' is pressed, the child function destroy() will be  executed.
         destroy()
+
+
+# import RPi.GPIO as GPIO
+# import time
+# import sys
+# from array import *
+
+# GPIO.setwarnings(False) 
+# GPIO.setmode(GPIO.BOARD)
+
+# steps    = int(sys.argv[1]);
+# clockwise = int(sys.argv[2]);
+
+# arr = [0,1,2,3];
+# if clockwise!=1:
+#     arr = [3,2,1,0];
+
+# ports = [40,38,36,35] # GPIO 21（Pin 40） GPIO 20（Pin 38） GPIO 16（Pin 36） GPIO 19（Pin 35）
+
+# for p in ports:
+#     GPIO.setup(p,GPIO.OUT)
+    
+# for x in range(0,steps):
+#     for j in arr:
+#         time.sleep(0.01)
+#         for i in range(0,4):
+#             if i == j:            
+#                 GPIO.output(ports[i],True)
+#             else:
+#                 GPIO.output(ports[i],False)
