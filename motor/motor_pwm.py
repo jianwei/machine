681fe2fb0052380febf27b6b4c48651fce90a246
Carlos  chen <ccjjww1222@gmail.com>
2022-07-19 14:50:41 +0800

    1
    2

--- motor/motor_pwm.py
+++ motor/motor_pwm.py
@@ -1,12 +1,13 @@
-class stepper_motor(object):
+import RPi.GPIO as GPIO
+class stepper_motor():
     '''
         Test script for step motor HW23-601 (https://www.mouser.com/c/?q=HW23-601) 
         with driver STR2 (https://www.applied-motion.com/products/stepper-drives/str2)
     '''
     num_of_steps_for_360 = 2000                 # this could be changed, please see the totorial of STR2/4
     resolution = num_of_steps_for_360 / 360     # resolution, how many steps would trigger 1 degree rotation
-    CW_control_port = 21                        # GPIO 21 for clockwise (physically connected to dir+ port in the driver)
-    CCW_control_port = 18                       # GPIO 18 for counter clockwise (physically connected to step+ port in the driver)
+    CW_control_port = 4                        # GPIO 21 for clockwise (physically connected to dir+ port in the driver)
+    CCW_control_port = 17                       # GPIO 18 for counter clockwise (physically connected to step+ port in the driver)
     inner_speed = 120                           # rotation speed: 120 rpm, outer would be 30 rpm
     gear_ratio = 4                              # radius of the outer gear is 4 times than the radius of the inner gear
     outer_speed = inner_speed / gear_ratio      # acutal speed: 30 rpm, that means it will take around 1 second to rotate 180 degree
@@ -15,13 +16,13 @@ class stepper_motor(object):
     delay = 0.00075
     name = 'stepper_motor'
 
-    def __init__(self, log):
-        self.log = log
+    def __init__(self):
+        # self.log = log
         GPIO.setmode(GPIO.BCM)                                    # set the GPIO mode  
         GPIO.setup(self.CCW_control_port, GPIO.OUT)               # set GPIO 18 as output
         GPIO.setup(self.CW_control_port, GPIO.OUT)                # set GPIO 21 as output
         time.sleep(0.5) 
-        self.log.info("stepper motor initialization accomplished, outer speed: %d rpm, resolution: %d steps/degree" % (self.outer_speed, self.resolution))
+        print("stepper motor initialization accomplished, outer speed: %d rpm, resolution: %d steps/degree" % (self.outer_speed, self.resolution))
 
     def __del__(self):
         GPIO.cleanup()
@@ -51,9 +52,14 @@ class stepper_motor(object):
                 self.send_pulse(num_of_steps, self.CW_control_port)
             else:
                 self.send_pulse(num_of_steps, self.CCW_control_port)
-            self.log.info("moved %d degree, direction:%s, current position: %d" % (angle, direction, self.cur_position))
+            print("moved %d degree, direction:%s, current position: %d" % (angle, direction, self.cur_position))
             print('move done')
             return True
         
     def get_motor_type(self):
         return self.name
+
+m = stepper_motor()
+m.send_pulse(50,23)
+m.move(360, 230)
+
