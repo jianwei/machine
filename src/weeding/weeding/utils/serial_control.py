7bb903f3a22fd54125048e1e9ca526d8b2ca8de8
Carlos  chen <ccjjww1222@gmail.com>
2022-07-26 10:58:30 +0800

    Update serial_control.py

--- src/weeding/weeding/utils/serial_control.py
+++ src/weeding/weeding/utils/serial_control.py
@@ -1,8 +1,9 @@
 #导入pyserial模块
 import serial,sys,os
+import redis
 sys.path.append(os.getcwd()+"/../../../../")
 from redisConn.index import redisDB
-redis = redisDB()              
+redis = redisDB()            
 class serial_control ():
     def __init__(self):
         self.ser = serial.Serial('/dev/ttyAMA0', 9600,timeout=0.5)
