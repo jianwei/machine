03d39c2882c6b544b6f8705cd0c5fbecdc4ed059
Carlos  chen <ccjjww1222@gmail.com>
2022-07-29 17:21:44 +0800

    2
    3

--- center/main.py
+++ center/main.py
@@ -9,15 +9,15 @@ from redisConn.index import redisDB
 class machine ():
     def __init__(self):
         self.redis = redisDB()
-        self.ser = serial.Serial('/dev/ttyAMA0', 9600,timeout=0.5)
+        # self.ser = serial.Serial('/dev/ttyAMA0', 9600,timeout=0.5)
         # self.convertPoints = ConvertPoints()
 
 
-    def send_cmd(self,msg):
-        self.ser.write(cmd)
+    def send_cmd(self,cmd):
+        # self.ser.write(cmd)
         try:    
-            response = self.ser.readall() #read a string from port
-            print ("serial_control:cmd",cmd,response)
+            # response = self.ser.readall() #read a string from port
+            print ("serial_control:cmd",cmd)
         except expression:
             print("serial_control,expression:",cmd,expression)
   
@@ -25,40 +25,41 @@ class machine ():
     def loop(self):
         while (True):
             value = self.redis.get("machine_cmd")
-            print("loop:",value)
+            print("loop1:",value,type(value))
             if(value):
                 value = json.loads(value)
+            print("loop2:",value,type(value))
             if(value):
                 if ("wheel" in value):
-                    if ("RST" in value.get("wheel")):
+                    if ( "RST" == value.get("wheel")):
                         self.send_cmd("RST")        # 复位，返回0执行成功
-                    if ("QMS" in value.get("wheel")):
+                    if ("QMS" == value.get("wheel")):
                         self.send_cmd("QMS")        # query movement status查询行进速度等信息
-                    if ("QOS" in value.get("wheel")):
+                    if ("QOS" == value.get("wheel")):
                         self.send_cmd("QOS")        #  query_operation_status查询滑轨所在的位置等
-                    if ("MF" in value.get("wheel")):
-                        self.send_cmd("MF "+value.get("wheel").get("speed"))       # move_forward以指定速度持续前进；返回0执行成功
-                    if ("MB" in value.get("wheel")):
-                        self.send_cmd("MB "+value.get("wheel").get("speed"))       #  move_backward以指定速度持续后退；返回0执行成功
-                    if ("STOP" in value.get("wheel")):
-                        self.send_cmd("MF "+value.get("wheel").get("type"))       #  type: 0全停；1刹车；2操作臂停止；返回0执行成功
-                    if ("TA" in value.get("wheel")):
-                        self.send_cmd("TA "+value.get("wheel").get("angle"))       # turn_angle，angle为int度数；返回0执行成功
+                    if ("MF" == value.get("wheel")):
+                        self.send_cmd("MF "+str(value.get("speed")))       # move_forward以指定速度持续前进；返回0执行成功
+                    if ("MB" == value.get("wheel")):
+                        self.send_cmd("MB "+str(value.get("speed")))       #  move_backward以指定速度持续后退；返回0执行成功
+                    if ("STOP" == value.get("wheel")):
+                        self.send_cmd("STOP "+str(value.get("type")))      #  type: 0全停；1刹车；2操作臂停止；返回0执行成功
+                    if ("TA" == value.get("wheel")):
+                        self.send_cmd("TA "+str(value.get("angle")))       # turn_angle，angle为int度数；返回0执行成功
 
                 if ("slide" in value):
-                    if ("ML" in value.get("slide")):
-                        self.send_cmd("MF "+value.get("slide").get("distance"))       # move_forward以指定速度持续前进；返回0执行成功
-                    if ("MR" in value.get("slide")):
-                        self.send_cmd("MR "+value.get("slide").get("distance"))       #  move_backward以指定速度持续后退；返回0执行成功
-                    if ("MU" in value.get("slide")):
-                        self.send_cmd("MU "+value.get("slide").get("distance"))       #  type: 0全停；1刹车；2操作臂停止；返回0执行成功
-                    if ("MD" in value.get("slide")):
-                        self.send_cmd("MD "+value.get("slide").get("distance"))       # turn_angle，angle为int度数；返回0执行成功
+                    if ("ML" == value.get("slide")):
+                        self.send_cmd("MF "+str(value.get("distance")))       # move_forward以指定速度持续前进；返回0执行成功
+                    if ("MR" == value.get("slide")):
+                        self.send_cmd("MR "+str(value.get("distance")))       #  move_backward以指定速度持续后退；返回0执行成功
+                    if ("MU" == value.get("slide")):
+                        self.send_cmd("MU "+str(value.get("distance")))       #  type: 0全停；1刹车；2操作臂停止；返回0执行成功
+                    if ("MD" == value.get("slide")):
+                        self.send_cmd("MD "+str(value.get("distance")))       # turn_angle，angle为int度数；返回0执行成功
                 if ("motion" in value):
                     if ("ROT" in value.get("motion")):
-                        self.send_cmd("ROT "+value.get("motion").get("angle"))       #  rotate按指定角度（int）旋转；返回0执行成功
+                        self.send_cmd("ROT "+str(value.get("angle")))       #  rotate按指定角度（int）旋转；返回0执行成功
                     if ("STOP" in value.get("motion")):
-                        self.send_cmd("STOP "+value.get("motion").get("type"))       #  0全停；1刹车；2操作臂停止；返回0执行成功
+                        self.send_cmd("STOP "+str(value.get("type")))       #  0全停；1刹车；2操作臂停止；返回0执行成功
             self.redis.set("machine_cmd","")
             time.sleep(5)
         # pass
---
+++ xbox/keyboard.py
@@ -0,0 +1,59 @@
+import sys,json
+from pathlib import Path
+from pynput import keyboard
+path = str(Path(__file__).resolve().parents[1])
+sys.path.append(path)
+from redisConn.index import redisDB
+redis = redisDB()
+# 前 w
+# 后 s 
+# 上 u
+# 下 i
+# 左 j
+# 右 k
+def on_press(key):
+    try:
+        cmd = {}
+        print('alphanumeric key {0} pressed'.format(key.char))
+        if (key.char=="w"): 
+            cmd["wheel"] =  "MF"
+            cmd["speed"] =  10
+        if (key.char=="s"): 
+            cmd["wheel"] =  "MB"
+            cmd["speed"] =  10
+        if (key.char=="u"): 
+            cmd["slide"] =  "ML"
+            cmd["distance"] =  10
+        if (key.char=="i"): 
+            cmd["wheel"] =  "MR"
+            cmd["distance"] =  10
+        if (key.char=="j"): 
+            cmd["wheel"] =  "MU"
+            cmd["distance"] =  10
+        if (key.char=="k"): 
+            cmd["wheel"] =  "MD"
+            cmd["distance"] =  10
+        print (cmd)
+        redis.set("machine_cmd", json.dumps(cmd))
+    except AttributeError:
+        print('special key {0} pressed'.format(
+            key))
+
+def on_release(key):
+    print('{0} released'.format(
+        key))
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+# Collect events until released
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
+
+# ...or, in a non-blocking fashion:
+listener = keyboard.Listener(
+    on_press=on_press,
+    on_release=on_release)
+listener.start()
