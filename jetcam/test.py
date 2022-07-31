e942c82ab9af3bd1c5ab8e943170eeedbe2507de
Carlos  chen <ccjjww1222@gmail.com>
2022-07-31 18:25:05 +0800

    1
    2

--- jetcam/test.py
+++ jetcam/test.py
@@ -9,7 +9,7 @@ if video_capture.isOpened():
     try:
         window_handle = cv2.namedWindow(window_title, cv2.WINDOW_AUTOSIZE)
         while True:
-            frame = video_capture._read()
+            frame = camera._read()
             if cv2.getWindowProperty(window_title, cv2.WND_PROP_AUTOSIZE) >= 0:
                 cv2.imshow(window_title, frame)
             else:
