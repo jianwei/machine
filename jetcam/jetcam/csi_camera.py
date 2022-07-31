22e603931cc4d03488b7bd066ed498495e1cda30
Carlos  chen <ccjjww1222@gmail.com>
2022-07-31 18:48:45 +0800

    1
    2

--- jetcam/jetcam/csi_camera.py
+++ jetcam/jetcam/csi_camera.py
@@ -30,7 +30,7 @@ class CSICamera(Camera):
                 
     def _gst_str(self):
         # return 'nvarguscamerasrc sensor-id=%d ! video/x-raw(memory:NVMM), width=%d, height=%d, format=(string)NV12, framerate=(fraction)%d/1 ! nvvidconv ! video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! videoconvert ! appsink' % (
-        return 'nvarguscamerasrc sensor-id=%d ! video/x-raw(memory:NVMM), width=%d, height=%d, format=(string)NV12, framerate=(fraction)%d/1 ! nvvidconv flip-method=1  ! video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! videoconvert ! appsink' % (
+        return 'nvarguscamerasrc sensor-id=%d ! video/x-raw(memory:NVMM), width=%d, height=%d, format=(string)NV12, framerate=(fraction)%d/1 ! nvvidconv flip-method=2  ! video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! videoconvert ! appsink' % (
                 self.capture_device, self.capture_width, self.capture_height, self.capture_fps, self.width, self.height)
     
     def _read(self):
