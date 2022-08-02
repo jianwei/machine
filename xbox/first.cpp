82b8656099d00f250bb61051d2cef56b2ad8fbad
Carlos  chen <ccjjww1222@gmail.com>
2022-08-02 13:30:43 +0800

    Update first.cpp

--- xbox/first.cpp
+++ xbox/first.cpp
@@ -243,7 +243,7 @@ int main(void)
             continue;
         }
 
-        printf("\rTime:%8d A:%d B:%d X:%d Y:%d LB:%d RB:%d start:%d back:%d home:%d LO:%d RO:%d XX:%-6d YY:%-6d LX:%-6d LY:%-6d RX:%-6d RY:%-6d LT:%-6d RT:%-6d",
+        printf("\rTime:%8d A:%d B:%d X:%d Y:%d LB:%d RB:%d start:%d back:%d home:%d LO:%d RO:%d XX:%d YY:%d LX:%d LY:%d RX:%d RY:%d LT:%d RT:%d",
                map.time, map.a, map.b, map.x, map.y, map.lb, map.rb, map.start, map.back, map.home, map.lo, map.ro,
                map.xx, map.yy, map.lx, map.ly, map.rx, map.ry, map.lt, map.rt);
 
