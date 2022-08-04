70878f2bfc147921641d40f1bb57be7aeef071fa
Carlos  chen <ccjjww1222@gmail.com>
2022-08-04 17:46:08 +0800

    Update track.py

--- StrongSORT/track.py
+++ StrongSORT/track.py
@@ -269,7 +269,7 @@ def run(
                                 save_one_box(bboxes, imc, file=save_dir / 'crops' / txt_file_name / names[c] / f'{id}' / f'{p.stem}.jpg', BGR=True)   
                 LOGGER.info(f'{s}Done. YOLO:({t3 - t2:.3f}s), StrongSORT:({t5 - t4:.3f}s)')
                 addPhoto(allPoints)
-                print("track.py--allPoints--:",allPoints)
+                # print("track.py--allPoints--:",allPoints)
             else:
                 strongsort_list[i].increment_ages()
                 LOGGER.info('No detections')
