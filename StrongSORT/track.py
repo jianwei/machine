63eaa3032db8df6a4a711241779f578f3f5af7cb
Carlos  chen <ccjjww1222@gmail.com>
2022-08-04 17:07:38 +0800

    Update track.py

--- StrongSORT/track.py
+++ StrongSORT/track.py
@@ -147,15 +147,15 @@ def run(
 
     # tt=1
     # print("line:149")
-    tt = 0
+    # tt = 0
     for frame_idx, (path, im, im0s, vid_cap, s) in enumerate(dataset):
-        tt+=1
-        print("time:",int(time.time()),tt)
-        if(tt>100):
-            break
-        # begin_work = redis.get("begin_work")
-        # if(begin_work and int(begin_work)==0):
+        # tt+=1
+        # if(tt>100):
         #     break
+        begin_work = redis.get("begin_work")
+        if(begin_work and int(begin_work)==0):
+            break
+        print("time:",int(time.time()),begin_work)
         
         t1 = time_sync()
         im = torch.from_numpy(im).to(device)
@@ -306,8 +306,9 @@ def run(
 
     # Print results
     print("seen:",seen)
-    t = tuple(x / seen * 1E3 for x in dt)  # speeds per image
-    LOGGER.info(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS, %.1fms strong sort update per image at shape {(1, 3, *imgsz)}' % t)
+    if seen:
+        t = tuple(x / seen * 1E3 for x in dt)  # speeds per image
+        LOGGER.info(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS, %.1fms strong sort update per image at shape {(1, 3, *imgsz)}' % t)
     if save_txt or save_vid:
         s = f"\n{len(list(save_dir.glob('tracks/*.txt')))} tracks saved to {save_dir / 'tracks'}" if save_txt else ''
         LOGGER.info(f"Results saved to {colorstr('bold', save_dir)}{s}")
