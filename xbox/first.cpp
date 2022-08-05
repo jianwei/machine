0337170d1f8958d813622808accf6ec3b27a64c6
Carlos  chen <ccjjww1222@gmail.com>
2022-08-05 13:37:05 +0800

    Update first.cpp

--- xbox/first.cpp
+++ xbox/first.cpp
@@ -355,9 +355,9 @@ int main(void)
             usleep(10 * 1000);
             continue;
         }
-        // printf("\rTime:%8d A:%d B:%d X:%d Y:%d LB:%d RB:%d start:%d back:%d home:%d LO:%d RO:%d XX:%-6d YY:%-6d LX:%-6d LY:%-6d RX:%-6d RY:%-6d LT:%-6d RT:%-6d",
-        //         map.time, map.a, map.b, map.x, map.y, map.lb, map.rb, map.start, map.back, map.home, map.lo, map.ro,
-        //         map.xx, map.yy, map.lx, map.ly, map.rx, map.ry, map.lt, map.rt);
+        printf("\rTime:%8d A:%d B:%d X:%d Y:%d LB:%d RB:%d start:%d back:%d home:%d LO:%d RO:%d XX:%-6d YY:%-6d LX:%-6d LY:%-6d RX:%-6d RY:%-6d LT:%-6d RT:%-6d",
+                map.time, map.a, map.b, map.x, map.y, map.lb, map.rb, map.start, map.back, map.home, map.lo, map.ro,
+                map.xx, map.yy, map.lx, map.ly, map.rx, map.ry, map.lt, map.rt);
 
         // printf("\rTime:%8d  LO:%d RO:%d  LX:%d LY:%d RX:%d RY:%d \r\n", map.time, map.lo, map.ro,map.lx, map.ly, map.rx, map.ry);
         //开始工作、停止工作
