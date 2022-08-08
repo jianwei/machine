2a42c44487484a6e308689f9e0c4620bba0b6329
Carlos  chen <ccjjww1222@gmail.com>
2022-08-08 15:45:17 +0800

    Update work.py

--- center/work.py
+++ center/work.py
@@ -8,15 +8,18 @@ class work:
 
     def loop(self):
         while(1):
-            tmie1 = time.time()
-            print("----------------------begin loop ------------------------------")
             
+            print("----------------------begin loop ------------------------------")
+            tmie1 = time.time()
+            print("tmie1:",tmie1)
             distance_pointer = self.redis.get("distance_pointer")
 
             print (distance_pointer)
-            print("time:",time.time())
-            print("----------------------end loop ------------------------------")
+            # print("time:",time.time())
             tmie2 = time.time()
+            print("tmie2:",tmie2)
+            print("----------------------end loop ------------------------------")
+            
             time.sleep(1)
 
 
@@ -25,6 +28,6 @@ if __name__ == "__main__":
     try:
         w.loop()
     except KeyboardInterrupt:
-        self.redis.set("distance_pointer",json.dumps([]))
+        self.redis.set("distance_pointer",json.dumps({}))
         print("ctrl+c stop")
         # m.send_cmd("STOP 0")
