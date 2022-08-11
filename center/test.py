970e667cd57f4c7c816d5b480cd93fafc44c7a90
Carlos  chen <ccjjww1222@gmail.com>
2022-08-11 16:57:44 +0800

    Update test.py

--- center/test.py
+++ center/test.py
@@ -17,12 +17,18 @@
 
 import serial    #import serial module
 ser = serial.Serial('/dev/ttyACM0', 9600,timeout=0.5);   #open named port at 9600,1s timeot
-
+ser.flushInput()
 #try and exceptstructure are exception handler
 try:
   while 1:
+    ser.println()
     ser.write('s'.encode());#writ a string to port
-    response = ser.readall();#read a string from port
+    size = ser.inWaiting()
+    if size != 0:
+        response = ser.read(size);
+        print(response)
+    # time.sleep(3)
+    # response = ser.readall();#read a string from port
     print("response:",response.decode('utf-8') )
 except:
   ser.close()
