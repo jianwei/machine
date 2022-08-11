fc52fd29f4bbc0acc0dbefa5834d5c47fb2567df
Carlos  chen <ccjjww1222@gmail.com>
2022-08-11 16:22:54 +0800

    Update test.py

--- center/test.py
+++ center/test.py
@@ -1,9 +1,9 @@
-import serial,json,sys,os
+import serial
 
 def main():
     str_cmd = "MF 10."
     ser = serial.Serial('/dev/ttyACM0', 9600,timeout=0.5)
-    ser.write(str_cmd)
+    ser.write(str_cmd.encode())
     try:    
         response = ser.readall().decode('utf-8')
         print("response",response)
