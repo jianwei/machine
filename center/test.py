d4903e5c4ca723257b5cb45d43bec228f7417e29
Carlos  chen <ccjjww1222@gmail.com>
2022-08-11 18:25:25 +0800

    Update test.py

--- center/test.py
+++ center/test.py
@@ -37,7 +37,7 @@ port = "/dev/ttyACM0" #Arduino端口
 ser = serial.Serial(port,9600,timeout=1) #设置端口，每秒回复一个信息
 ser.flushInput() #清空缓冲器
 
-ser.write(b'MF 5.') #将'1'字符转换为字节发送
+ser.write(b'MF 10.') #将'1'字符转换为字节发送
 try:
 	while True:
 		response = ser.readall()
