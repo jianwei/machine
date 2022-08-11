50ddb4663bcaf87255e7e03c51d24143ce47e12f
Carlos  chen <ccjjww1222@gmail.com>
2022-08-11 17:34:21 +0800

    Update test.py

--- center/test.py
+++ center/test.py
@@ -5,7 +5,7 @@
 #     ser = serial.Serial('/dev/ttyACM0', 9600,timeout=0.5)
 #     ser.write(str_cmd.encode())
 #     try:
-#         while 1:    
+#         while 1:
 #             response = ser.readall().decode('utf-8')
 #             print("response",response)
 #     except expression:
@@ -15,21 +15,19 @@
 # if __name__ == "__main__":
 #     main()
 
-import serial    #import serial module
-ser = serial.Serial('/dev/ttyACM0', 9600,timeout=0.5);   #open named port at 9600,1s timeot
+import serial  # import serial module
+ser = serial.Serial('/dev/ttyACM0', 9600, timeout=0.5)
+ser.open()
 open = ser.isOpen()
-print("isopen1:",open)
-#try and exceptstructure are exception handler
+print("isopen1:", open)
 try:
-  while 1:
-    # w = ser.write('s----'.encode());#writ a string to port
-    str_cmd = "MF 10."
-    w = ser.write(str_cmd.encode());#writ a string to port
-    print("isopen,w:",open,w)
-    # ser.write(str(chr(10)));#writ a string to port
-    response = ser.readall();#read a string from port
-    print("response:",response)
-    # print("response:",response.decode('utf-8') )
+    while 1:
+        str_cmd = b'MF 10.'
+        w = ser.write(str_cmd)
+        print("isopen,w:", open, w)
+        response = ser.readall()
+        print("response:", response)
+        # print("response:",response.decode('utf-8') )
 except Exception as e:
-    print("exception:",e)
+    print("exception:", e)
     ser.close()
