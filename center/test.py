f849ff8ada28123e7579423db951955f6c97fd3e
Carlos  chen <ccjjww1222@gmail.com>
2022-08-11 18:11:23 +0800

    Update test.py

--- center/test.py
+++ center/test.py
@@ -1,3 +1,37 @@
+# import serial
+
+# def main():
+#     str_cmd = "MF 10."
+#     ser = serial.Serial('/dev/ttyACM0', 9600,timeout=0.5)
+#     ser.write(str_cmd.encode())
+#     try:
+#         while 1:
+#             response = ser.readall().decode('utf-8')
+#             print("response",response)
+#     except expression:
+#         print("serial_control,expression:",cmd,expression)
+#     pass
+
+# if __name__ == "__main__":
+#     main()
+
+# import serial  # import serial module
+# ser = serial.Serial('/dev/ttyACM0', 9600, timeout=0.5)
+# open = ser.isOpen()
+# print("isopen1:", open)
+# try:
+#     while 1:
+#         str_cmd = b'MF 10.'
+#         w = ser.write(str_cmd)
+#         print("isopen,w:", open, w)
+#         response = ser.readall()
+#         print("response:", response)
+#         # print("response:",response.decode('utf-8') )
+# except Exception as e:
+#     print("exception:", e)
+#     ser.close()
+
+
 import serial #导入serial模块
 port = "/dev/ttyACM0" #Arduino端口
 ser = serial.Serial(port,9600,timeout=1) #设置端口，每秒回复一个信息
@@ -8,6 +42,7 @@ try:
 		ser.write(b'1') #将'1'字符转换为字节发送
 		response = ser.read()
 		print(var(response))
-except:
-	print("连接失败！")
+except Exception as e:
+	print("连接失败！",e)
 	ser.close()	#关闭端口
+
