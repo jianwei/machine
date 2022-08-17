9c1edfd9986321e3cfc3ce6bb8cf00b9cc20d512
Carlos  chen <ccjjww1222@gmail.com>
2022-08-17 18:19:52 +0800

    1
    2

--- center/main.py
+++ center/main.py
@@ -66,7 +66,7 @@ class machine ():
         currentTime = 0
         while (1):
             self.logger.info("----------------------loop begin ------------------------------")
-            # allPhoto = self.redis.get("allPoints")
+            allPhoto = self.redis.get("allPoints")
             global_angle = self.redis.get("global_angle")
             global_angle = int(global_angle) if global_angle else 90
             # allPhoto = json.dumps(mock)
