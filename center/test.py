ae438d981a8cd80fed040dc4ccbd59cf728d173e
Carlos  chen <ccjjww1222@gmail.com>
2022-08-17 14:03:59 +0800

    Update test.py

--- center/test.py
+++ center/test.py
@@ -11,7 +11,7 @@ if __name__ == '__main__':
     logger = l.getLogger()
     msg = {
         "uuid": str(uuid.uuid1()),
-        "cmd": "TR 90."
+        "cmd": "TR 10."
     }
     message = json.dumps(msg)
     logger.info("sendMsg:%s", message)
