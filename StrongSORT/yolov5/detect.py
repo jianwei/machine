cd6cc2d80f8c912b425d741461040912bb654d44
Carlos  chen <ccjjww1222@gmail.com>
2022-08-20 14:51:38 +0800

    Update detect.py

--- StrongSORT/yolov5/detect.py
+++ StrongSORT/yolov5/detect.py
@@ -95,8 +95,8 @@ def run(
 
     # Dataloader
     if webcam:
-        view_img = check_imshow()
-        # view_img = True
+        # view_img = check_imshow()
+        view_img = True
 
         cudnn.benchmark = True  # set True to speed up constant image size inference
         # dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt,capture_device=0)
