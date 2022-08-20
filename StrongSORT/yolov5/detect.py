26d12680b09c685d60b891b18f772e5cd64eb0fc
Carlos  chen <ccjjww1222@gmail.com>
2022-08-20 14:39:50 +0800

    Update detect.py

--- StrongSORT/yolov5/detect.py
+++ StrongSORT/yolov5/detect.py
@@ -99,7 +99,8 @@ def run(
         # view_img = True
 
         cudnn.benchmark = True  # set True to speed up constant image size inference
-        dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt,capture_device=0)
+        # dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt,capture_device=0)
+        dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt)
         bs = len(dataset)  # batch_size
     else:
         dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt)
