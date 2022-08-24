1f9ee8b00d1e469ff85c3ffd5e4ad13f5e2f9e21
Carlos  chen <ccjjww1222@gmail.com>
2022-08-24 14:49:29 +0800

    1

--- center/test.py
+++ center/test.py
@@ -11,8 +11,8 @@ if __name__ == '__main__':
     logger = l.getLogger()
     msg = {
         "uuid": str(uuid.uuid1()),
-        # "cmd": "RROT 100."
-        "cmd": "STOP RST."
+        "cmd": "RROT 100."
+        # "cmd": "STOP RST."
     }
     message = json.dumps(msg)
     logger.info("sendMsg:%s", message)
