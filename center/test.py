33b3ecf11afff3a3900c3e6c09b9e9e5ccda993f
Carlos  chen <ccjjww1222@gmail.com>
2022-08-24 13:12:18 +0800

    Update test.py

--- center/test.py
+++ center/test.py
@@ -4,18 +4,9 @@ import json
 from utils.log import log
 
 rmq = RMQ(url='redis://127.0.0.1:6379/15', name='arduino')
-import threading,time
 
-def setTimeout(cbname,delay,*argments):
-    threading.Timer(delay,cbname,argments).start()
-
-
-def b(arg):
-    print ("funb:",arg,time.time())
 
 if __name__ == '__main__':
-    print ("main:",time.time())
-    setTimeout(b,1,"----barg---")
     l = log()
     logger = l.getLogger()
     msg = {
@@ -26,5 +17,3 @@ if __name__ == '__main__':
     message = json.dumps(msg)
     logger.info("sendMsg:%s", message)
     print(rmq.publish(message))
-    # print(rmq.publish('RST.'))
-    # rmq.run_subscribe()
