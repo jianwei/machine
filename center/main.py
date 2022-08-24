c51f6db018d6fc7c3f6586c4c4dfe0b15938d345
Carlos  chen <ccjjww1222@gmail.com>
2022-08-24 18:38:08 +0800

    Update main.py

--- center/main.py
+++ center/main.py
@@ -54,7 +54,7 @@ def setTimeout(cbname,delay,*argments):
 
 
 def wheel():
-    send("STOP 1")
+    send("STOP 0")
     send("RROT 100")
     min_time = 1.225  # 1秒 1.225圈
     unit = 1/min_time  # 1圈  unit 秒
