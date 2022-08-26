303bf6c32957085ed66841a398893b60c075449f
Carlos  chen <ccjjww1222@gmail.com>
2022-08-26 15:44:41 +0800

    Update dataloaders.py

--- StrongSORT/yolov5/utils/dataloaders.py
+++ StrongSORT/yolov5/utils/dataloaders.py
@@ -303,7 +303,7 @@ class LoadWebcam:  # for inference
 class LoadStreams:
     # YOLOv5 streamloader, i.e. `python detect.py --source 'rtsp://example.com/media.mp4'  # RTSP, RTMP, HTTP streams`
     # def __init__(self, sources='streams.txt', img_size=640, stride=32, auto=True,capture_device=0):
-    def __init__(self, sources='streams.txt', img_size=640, stride=32, auto=True, USBCamera = True):
+    def __init__(self, sources='streams.txt', img_size=640, stride=32, auto=True, isUSBCamera = True):
         self.mode = 'stream'
         self.img_size = img_size
         self.stride = stride
@@ -318,7 +318,7 @@ class LoadStreams:
         self.imgs, self.fps, self.frames, self.threads = [None] * n, [0] * n, [0] * n, [None] * n
         self.sources = [clean_str(x) for x in sources]  # clean source names for later
         self.auto = auto
-        if(USBCamera):
+        if(isUSBCamera):
             camera = USBCamera(capture_device=1)
         else:
             camera = CSICamera(width=1080, height=720, capture_width=1080, capture_height=720, capture_fps=30)
