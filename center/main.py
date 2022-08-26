87a21dd044163b12b3af501de810bbd8609f460e
Carlos  chen <ccjjww1222@gmail.com>
2022-08-26 11:41:49 +0800

    Update main.py

--- center/main.py
+++ center/main.py
@@ -60,7 +60,7 @@ def wheel():
     # min_time = 0.25  # 1秒 1.225圈
     # unit = 1/min_time-0.02  # 1圈unit秒 , 0.02误差时间,可调整
     # main_logger.info("send RROT 100:%s", time.time())
-    min_time = 2.2  # 1秒 1.225圈
+    min_time = 2.0  # 1秒 1.225圈
     unit = 1/min_time-0.02  # 1圈unit秒 , 0.02误差时间,可调整
     next_cmd = [
         {
