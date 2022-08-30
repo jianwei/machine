e4c0845c9179a3e9b8c4a46ddc26cc02b1d876de
Carlos  chen <ccjjww1222@gmail.com>
2022-08-30 17:02:50 +0800

    Update main_wheel.py

--- center/main_wheel.py
+++ center/main_wheel.py
@@ -1,5 +1,6 @@
 # from convertPoints import ConvertPoints
 
+# from math import fabs
 from utils.redis_message_queue import RMQ
 import time
 import os
@@ -7,51 +8,87 @@ import sys
 import json
 import uuid
 import numpy
-# import serial
+import random
 from utils.speed import speed
 from utils.point import point
 from utils.line import line
 from utils.work import work
 from utils.log import log
-sys.path.append("..")
-from redisConn.index import redisDB
-# chmod -R 777 /dev/ttyAMA0
-# import serial,sys,os,redis,time
+from utils.serial_control import serial_control
 
+
+from pathlib import Path
+path = str(Path(__file__).resolve().parents[1])
+sys.path.append(path)
+from redisConn.index import redisDB
+
+
+
+
+
+mainlog = log("main.log")
+main_logger = mainlog.getLogger()
+main_pub_rmq = RMQ(url='redis://127.0.0.1:6379', name='arduino')
+redis = redisDB()
+redis.set("is_working",0)
+ser =  serial_control()
+# is_working = False
+
+def send(cmd):
+    if(cmd!=""):
+        cmd += "."
+        cmd_dict = {
+            "uuid": str(uuid.uuid1()),
+            "cmd": cmd,
+            "from": "camera",
+        }
+        ser.send_cmd(cmd_dict)
+    else:
+        main_logger.info("cmd null")
+
+
+
+def wheel(speed):
+    rot_speed = 60
+    unit_sleep = 1/(rot_speed*50/2/1000)   #转1圈所需要的时间
+    unit_sleep += 0.02    #误差
+    print("unit_sleep",unit_sleep)
+
+    send("STOP 0")
+    send("MD")
+    time.sleep(2)
+    send("STOP 2")
+    send("RROT "+str(rot_speed))
+    time.sleep(unit_sleep)
+    send("STOP 2")
+    send("MU")
+    time.sleep(2)
+    send("STOP 2")
+    redis.set("is_working",0)
+    send("MF "+str(speed))
+
+    
 
 class machine ():
     def __init__(self):
         self.redis = redisDB()
-        # self.default_speed = 10
-        self.pub_rmq = RMQ(url='redis://127.0.0.1:6379', name='arduino')
+        self.pub_rmq = main_pub_rmq
         self.point = point()
         self.speed = speed(self.point)
         self.line = line(self.point)
-        self.l = log("main.log")
-        self.work = work(self.point, self.speed,self.l)
+        self.work = work(self.point, self.speed,main_logger)
         self.angle_distance = 5  # cm
-        
-        self.logger = self.l.getLogger()
-        # self.ser = serial.Serial('/dev/ttyAMA0', 9600,timeout=0.5)
-        # self.convertPoints = ConvertPoints()
+        self.logger = main_logger
+
 
     def send_cmd(self, cmd):
-        if(cmd!=""):
-            cmd += "."
-            cmd_dict = {
-                "uuid": str(uuid.uuid1()),
-                "cmd": cmd,
-                "from": "camera"
-            }
-            self.logger.info("send_cmd-cmd:%s", cmd_dict)
-            self.pub_rmq.publish(json.dumps(cmd_dict))
-        else:
-            self.logger.info("cmd null")
-        # response = self.ser.readall() #read a string from port
+        send(cmd)
 
     def loop(self):
+        randId = random.random()
         mock = [
             [
+                {"point": [[470, 466], [550, 466], [470, 626], [550, 626]], "id": randId, "name": "bottle", "time": 1661318695.8770459, "screenSize": [1080, 720], "uuid": "16c50720-236d-11ed-929a-1cbfc0958bef", "centerx": 510.0, "centery": 546.0, "center": [510.0, 546.0]},
                 #  {'point': [[110, 145], [230, 145], [110, 165], [230, 165]], 'id': 1, 'name': 'person', 'time': 1659515712.9082823, 'screenSize': [1080, 720]},
                 #  {'point': [[410, 145], [530, 145], [410, 165], [530, 165]], 'id': 2, 'name': 'person', 'time': 1659515712.9082823, 'screenSize': [1080, 720]},
                 #  {'point': [[800, 145], [930, 145], [800, 165], [930, 165]], 'id': 3, 'name': 'person', 'time': 1659515712.9082823, 'screenSize': [1080, 720]},
@@ -61,55 +98,133 @@ class machine ():
                 #  {'point': [[110, 645], [230, 645], [110, 715], [230, 715]], 'id': 7, 'name': 'person', 'time': 1659515712.9082823, 'screenSize': [1080, 720]},
                 #  {'point': [[410, 645], [530, 645], [410, 715], [530, 715]], 'id': 8, 'name': 'person', 'time': 1659515712.9082823, 'screenSize': [1080, 720]},
                 # {'point': [[200, 645], [330, 645], [200, 715], [330, 715]], 'id': 9,'name': 'person', 'time': 1659515712.9082823, 'screenSize': [1080, 720]}
-                {"point": [[736, 0], [959, 0], [736, 548], [959, 548]], "id": 182, "name": "person", "time": 1660786080.6786768, "screenSize": [1080, 720],"center":[847.5,274],"centerx":847.5,"centery":274},
-                {"point": [[736, 650], [959, 650], [736, 710], [959, 710]], "id": 182, "name": "person", "time": 1660786080.6786768, "screenSize": [1080, 720],"center":[847.5,660],"centerx":847.5,"centery":660}
+                {"point": [[736, 0], [959, 0], [736, 548], [959, 548]], "id": randId, "name": "person", "time": 1660786080.6786768, "screenSize": [1080, 720],"center":[847.5,274],"centerx":847.5,"centery":274},
+                {"point": [[736, 650], [959, 650], [736, 710], [959, 710]], "id": randId, "name": "person", "time": 1660786080.6786768, "screenSize": [1080, 720],"center":[847.5,660],"centerx":847.5,"centery":660}
             ],
             [
-                {"point": [[736, 10], [959, 10], [736, 558], [959, 558]], "id": 182, "name": "person", "time": 1660786079.6786768, "screenSize": [1080, 720],"center":[847.5,284],"centerx":847.5,"centery":284},
-                {"point": [[736, 660], [959, 660], [736, 720], [959, 720]], "id": 182, "name": "person", "time": 1660786079.6786768, "screenSize": [1080, 720],"center":[847.5,670],"centerx":847.5,"centery":670}
+                {"point": [[736, 10], [959, 10], [736, 558], [959, 558]], "id": randId, "name": "person", "time": 1660786079.6786768, "screenSize": [1080, 720],"center":[847.5,284],"centerx":847.5,"centery":284},
+                {"point": [[736, 660], [959, 660], [736, 720], [959, 720]], "id": randId, "name": "person", "time": 1660786079.6786768, "screenSize": [1080, 720],"center":[847.5,670],"centerx":847.5,"centery":670}
             ],
         ]
+        # {"point": [[302, 221], [434, 221], [302, 378], [434, 378]], "id": 229, "name": "cup", "time": 1661393590.437084, "screenSize": [1080, 720], "centerx": 368.0, "centery": 299.5, "center": [368.0, 299.5]}
         # print(mock)
         currentTime = 0
-        
-        self.logger.info("----------------------loop begin ------------------------------")
-        allPhoto = self.redis.get("allPoints")
-        global_angle = self.redis.get("global_angle")
-        global_angle = int(global_angle) if global_angle else 90
-        allPhoto = json.dumps(mock)
-        # work_flag = self.redis.get("begin_work")
-        # self.logger.info(allPhoto)
-        work_flag = 1
-        if (work_flag and int(work_flag) == 1):
-            if (allPhoto):
-                allPhoto = json.loads(allPhoto)
-                if (len(allPhoto) > 0):
-                    latsTime = allPhoto[0][0]["time"]
-                    screenSize = allPhoto[0][0]["screenSize"]
-                    if (latsTime == currentTime):
-                        self.logger.info("current latsTime:%s,loop",latsTime )
-                        time.sleep(0.1)
-                        # continue
-                    if (line and line[0]):
-                        lastLine  =  len (line)
-                        y = line[lastLine-1][0]["centery"]
-                        if (y >= 650 and y <= 720):
-                            workcmd = self.work.work(line,machine_speed)
-                            if (len(workcmd) > 0):
-                                self.send_cmd(workcmd)
-                                min_time = 1.225  # 1秒 1.225圈
-                                unit = 1/min_time  # 1圈  unit 秒
-                                time.sleep(unit)
-                                self.send_cmd("STOP 3.")
-                                # max_speed = 255
-                    
+        # global is_working
+        is_working = self.redis.get("is_working")
+        a=0
+        while (1):
+            a+=1
+            self.logger.info("----------------------loop begin ------------------------------%s",a)
+            allPhoto = self.redis.get("allPoints")
+            global_angle = self.redis.get("global_angle")
+            global_angle = int(global_angle) if global_angle else 90
+            self.logger.info("is_working:%s", is_working)
+            # allPhoto = json.dumps(mock)
+            # work_flag = self.redis.get("begin_work")
+            # self.logger.info(allPhoto)
+            work_flag = 1
+            if (work_flag and int(work_flag) == 1):
+                if (allPhoto):
+                    allPhoto = json.loads(allPhoto)
+                    if (len(allPhoto) > 0):
+                        latsTime = allPhoto[0][0]["time"]
+                        screenSize = allPhoto[0][0]["screenSize"]
+                        if (latsTime == currentTime):
+                            self.logger.info("current latsTime:%s,loop",latsTime )
+                            time.sleep(0.1)
+                            # continue
+                        currentTime = latsTime
+
+                        machine_speed = self.speed.getSpeed(allPhoto)
+                        self.logger.info("machine_speed:%s", machine_speed)
+                         # 分行 工作
+                        line = self.line.convertLine(allPhoto,0)
+                        if (line and line[0]):
+                            # print(4)
+                            lastLine  =  len (line)
+                            y = line[lastLine-1][0]["centery"]
+                            uuid_id = "vegetable-"+str(line[lastLine-1][0]["id"])
+                            if (self.redis.get(uuid_id)==str(1)) :
+                                self.logger.info("id 存在,1分钟内不重复处理:%s,%s,%s", uuid_id,self.redis.get(uuid_id),self.redis.get(uuid_id)==str(1))
+                                self.logger.info("------id,centery:%s,%s", uuid_id,y)
+                            else:
+                                self.redis.set("is_working",1)
+                                self.logger.info("id,centery:%s,%s", uuid_id,y)
+                                # "center": [217.5, 366.0]
+                                # "center": [197.0, 259.5]
+                                # ponit_y = 366  #中心点
+                                # if (y >= (ponit_y-15)):
+                                self.redis.set(uuid_id,1,10)
+                                workcmd = self.work.work(line,machine_speed)
+                                if (len(workcmd) > 0):
+                                    wheel(self.speed.revolution)
+                                else:
+                                    self.logger.info("------id,centery:%s,%s", uuid_id,y)
+                        if (is_working==0 or is_working=="0"):
+                            # self.logger.info("false-------------------is_working----------------------------------------:%s", is_working)
+                            #  稳定速度 转速
+                            
+                            revolution = self.speed.uniformSpeed(machine_speed)
+                            self.logger.info("revolution:%s", revolution)
+                            # if(revolution>=40)
+                            revolution = 40 if revolution>=40 else revolution
+                            self.go(revolution)
+                        
+                            # 左右位置调整
+                            # self.logger.info("line:%s", json.dumps(line))
+                            # if (line and len(line) > 0):
+                            #     center_point = screenSize[0]/2
+                            #     first = line[0]
+                            #     length = len(first)
+                            #     cmd = ""
+                            #     if (length > 0):
+                            #         if (length == 1 or length == 3):
+                            #             center = first[0] if length == 1 else first[1]
+                            #             centerx = center["centerx"]
+                            #             diff_point_x = centerx-center_point
+
+                            #             self.point.setScreenSize(screenSize)
+                            #             x= self.point.sizexm(abs(diff_point_x))
+                            #             tan = x/(1000*self.point.f)
+                            #             angle = int(numpy.arctan(tan) * 180.0 / 3.1415926)
+
+                            #             # print("angle,tan,x,diff_point_x,centerx,center_point:",angle,tan,x,diff_point_x,centerx,center_point)
+
+                            #             cmd_prefix = ""
+                            #             target_angle = 90
+                            #             if(global_angle<=90):
+                            #                 if (centerx<=center_point) :
+                            #                     target_angle = 90-angle
+                            #                     cmd_prefix = "TR" if global_angle<target_angle else "TL"
+                            #                 else:
+                            #                     target_angle = 90+angle
+                            #                     cmd_prefix = "TR"
+                            #             else:
+                            #                 if (centerx<=center_point) :
+                            #                     target_angle = 90-angle
+                            #                     cmd_prefix = "TL"
+                            #                 else:
+                            #                     target_angle = 90+angle
+                            #                     cmd_prefix = "TR" if global_angle<target_angle else "TL"
+                            #             # print("target_angle,global_angle5",target_angle,global_angle)
+                            #             if(target_angle!=global_angle):
+                            #                 cmd = cmd_prefix + " " + str(abs(target_angle-global_angle))
+                            #                 global_angle = target_angle
+                            #                 print ("send-cmd:",cmd)
+                            #             else:
+                            #                 print ("send-cmd:none")
+                            #             # print("target_angle,global_angle2",target_angle,global_angle)
+                            #             self.redis.set("global_angle", global_angle)
+                            #             self.send_cmd(cmd)
+                    else:
+                        self.go(self.speed.revolution)
                 else:
                     self.go(self.speed.revolution)
             else:
-                self.go(self.speed.revolution)
-        else:
-            self.redis.set("allPoints", json.dumps([]))
-        self.logger.info("time:%s,begin_work:%s", time.time(), work_flag)
+                self.redis.set("allPoints", json.dumps([]))
+            self.logger.info("time:%s,begin_work:%s", time.time(), work_flag)
+            self.logger.info("----------------------loop end ------------------------------")
+            time.sleep(1)
 
     def go(self,revolution):
         # revolution = self.speed.revolution
