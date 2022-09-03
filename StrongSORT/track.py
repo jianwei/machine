25c1d6f33d9d873aa0b215f8888030d9d4c4f832
Carlos  chen <ccjjww1222@gmail.com>
2022-09-03 15:22:52 +0800

    Update track.py

--- StrongSORT/track.py
+++ StrongSORT/track.py
@@ -1,5 +1,4 @@
 import argparse
-import json
 
 import os
 # limit the number of cpus used by high performance libraries
@@ -12,10 +11,8 @@ os.environ["NUMEXPR_NUM_THREADS"] = "1"
 import sys
 import numpy as np
 from pathlib import Path
-import torch,time
+import torch
 import torch.backends.cudnn as cudnn
-# import uuid
-import threading
 
 FILE = Path(__file__).resolve()
 ROOT = FILE.parents[0]  # yolov5 strongsort root directory
@@ -39,16 +36,8 @@ from yolov5.utils.plots import Annotator, colors, save_one_box
 from strong_sort.utils.parser import get_config
 from strong_sort.strong_sort import StrongSORT
 
-# path = str(Path(__file__).resolve().parents[1])
-# sys.path.append(path)
-# from redisConn.index import redisDB
-# from center.utils.point import point
-from yolov5.utils.work import work
-
 # remove duplicated stream handler to avoid duplicated logging
 logging.getLogger().removeHandler(logging.getLogger().handlers[0])
-# redis = redisDB()
-# point = point()
 
 @torch.no_grad()
 def run(
@@ -81,9 +70,8 @@ def run(
         hide_class=False,  # hide IDs
         half=False,  # use FP16 half-precision inference
         dnn=False,  # use OpenCV DNN for ONNX inference
-        capture_device=0,  # use OpenCV DNN for ONNX inference
 ):
-    # redis.set("allPoints",json.dumps([]))
+
     source = str(source)
     save_img = not nosave and not source.endswith('.txt')  # save inference images
     is_file = Path(source).suffix[1:] in (VID_FORMATS)
@@ -94,12 +82,12 @@ def run(
 
     # Directories
     if not isinstance(yolo_weights, list):  # single yolo model
-        exp_name = yolo_weights.stem
+        exp_name = str(yolo_weights).rsplit('/', 1)[-1].split('.')[0]
     elif type(yolo_weights) is list and len(yolo_weights) == 1:  # single models after --yolo_weights
-        exp_name = Path(yolo_weights[0]).stem
+        exp_name = yolo_weights[0].split(".")[0]
     else:  # multiple models after --yolo_weights
         exp_name = 'ensemble'
-    exp_name = name if name else exp_name + "_" + strong_sort_weights.stem
+    exp_name = name if name is not None else exp_name + "_" + str(strong_sort_weights).split('/')[-1].split('.')[0]
     save_dir = increment_path(Path(project) / exp_name, exist_ok=exist_ok)  # increment run
     (save_dir / 'tracks' if save_txt else save_dir).mkdir(parents=True, exist_ok=True)  # make dir
 
@@ -110,15 +98,11 @@ def run(
     imgsz = check_img_size(imgsz, s=stride)  # check image size
 
     # Dataloader
-    screenSize = [640,480]
     if webcam:
         show_vid = check_imshow()
-        # show_vid = False
         cudnn.benchmark = True  # set True to speed up constant image size inference
-        dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt, capture_device = capture_device)
-        # dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt,isUSBCamera = True,capture_device=2)
+        dataset = LoadStreams(source, img_size=imgsz, stride=stride, auto=pt)
         nr_sources = len(dataset)
-        screenSize = dataset.getScreen()
     else:
         dataset = LoadImages(source, img_size=imgsz, stride=stride, auto=pt)
         nr_sources = 1
@@ -151,16 +135,7 @@ def run(
     model.warmup(imgsz=(1 if pt else nr_sources, 3, *imgsz))  # warmup
     dt, seen = [0.0, 0.0, 0.0, 0.0], 0
     curr_frames, prev_frames = [None] * nr_sources, [None] * nr_sources
-
-    
     for frame_idx, (path, im, im0s, vid_cap, s) in enumerate(dataset):
-        # begin_work = redis.get("begin_work")
-        # print("time:",int(time.time()),begin_work)
-        # if(begin_work and int(begin_work)==0):
-        #     break
-        total_predictions =0
-        total_time = 0
-        
         t1 = time_sync()
         im = torch.from_numpy(im).to(device)
         im = im.half() if half else im.float()  # uint8 to fp16/32
@@ -171,17 +146,16 @@ def run(
         dt[0] += t2 - t1
 
         # Inference
-        visualize = increment_path(save_dir / Path(path[0]).stem, mkdir=True) if visualize else False
-        pred = model(im, augment=augment, visualize=visualize)
+        visualize = increment_path(save_dir / Path(path[0]).stem, mkdir=True) if opt.visualize else False
+        pred = model(im, augment=opt.augment, visualize=visualize)
         t3 = time_sync()
         dt[1] += t3 - t2
 
         # Apply NMS
-        pred = non_max_suppression(pred, conf_thres, iou_thres, classes, agnostic_nms, max_det=max_det)
+        pred = non_max_suppression(pred, opt.conf_thres, opt.iou_thres, opt.classes, opt.agnostic_nms, max_det=opt.max_det)
         dt[2] += time_sync() - t3
 
         # Process detections
-        is_need_done = False
         for i, det in enumerate(pred):  # detections per image
             seen += 1
             if webcam:  # nr_sources >= 1
@@ -210,7 +184,6 @@ def run(
             annotator = Annotator(im0, line_width=2, pil=not ascii)
             if cfg.STRONGSORT.ECC:  # camera motion compensation
                 strongsort_list[i].tracker.camera_update(prev_frames[i], curr_frames[i])
-            
 
             if det is not None and len(det):
                 # Rescale boxes from img_size to im0 size
@@ -231,11 +204,10 @@ def run(
                 t5 = time_sync()
                 dt[3] += t5 - t4
 
-                allPoints = []
-                # scan_arr = ["cup","bottle","clock","mouse","person"]
                 # draw boxes for visualization
                 if len(outputs[i]) > 0:
                     for j, (output, conf) in enumerate(zip(outputs[i], confs)):
+    
                         bboxes = output[0:4]
                         id = output[4]
                         cls = output[5]
@@ -251,58 +223,21 @@ def run(
                                 f.write(('%g ' * 10 + '\n') % (frame_idx + 1, id, bbox_left,  # MOT format
                                                                bbox_top, bbox_w, bbox_h, -1, -1, -1, i))
 
-                        # if save_vid or save_crop or show_vid:  # Add bbox to image
-                        if True:  # Add bbox to image
+                        if save_vid or save_crop or show_vid:  # Add bbox to image
                             c = int(cls)  # integer class
                             id = int(id)  # integer id
                             label = None if hide_labels else (f'{id} {names[c]}' if hide_conf else \
                                 (f'{id} {conf:.2f}' if hide_class else f'{id} {names[c]} {conf:.2f}'))
-                            
-                            # if names[c] !="cup" or names[c] !="bottle":
-                                # continue 
-                             
-                            box_label = annotator.box_label(bboxes, label, color=colors(c, True))
-                            
-                            
-                            # if  names[c]  in scan_arr:
-                            #     box_label = annotator.set_redis_data(box_label,names[c],screenSize)
-                            #     allPoints.append(box_label)
-                            #     is_need_done = True
-
-                                # if names[c] in ["person","cup"]:
-                                    
-                                    # allPoints.append(box_label)
-
+                            annotator.box_label(bboxes, label, color=colors(c, True))
                             if save_crop:
                                 txt_file_name = txt_file_name if (isinstance(path, list) and len(path) > 1) else ''
                                 save_one_box(bboxes, imc, file=save_dir / 'crops' / txt_file_name / names[c] / f'{id}' / f'{p.stem}.jpg', BGR=True)
-                if webcam:
-                    frame_time = time_sync() - t1
-                    total_time += frame_time
-                    total_predictions += 1
-                    LOGGER.info(f'{s}Done. YOLO:({t3 - t2:.3f}s), StrongSORT:({t5 - t4:.3f}s),FPS: {1/frame_time},Avg FPS: {total_predictions/total_time},time:{time.time()}')
-                else:
-                    LOGGER.info(f'{s}Done. YOLO:({t3 - t2:.3f}s), StrongSORT:({t5 - t4:.3f}s)')
-                
-                work_obj = work()
-                if(is_need_done):
-                    print("-----------------------------------------work begin--track-----------------------------------------")
-                    if (not work_obj.is_lock()) :
-                        work_obj.mk_lock_file()
-                        setTimeout(work_obj.wheel,0.00001,"15")
-                    print("-----------------------------------------work end --track-----------------------------------------")
 
+                LOGGER.info(f'{s}Done. YOLO:({t3 - t2:.3f}s), StrongSORT:({t5 - t4:.3f}s)')
 
-
-                key ="allPoints"  if capture_device == 0 else "navigation_points"
-                print("key:",key)
-                # annotator.addPhoto(key,allPoints,redis)
-                # annotator.addPhoto("allPoints",allPoints,redis)
-                # annotator.addPhoto("navigation_points",allPoints,redis)
             else:
                 strongsort_list[i].increment_ages()
                 LOGGER.info('No detections')
-            
 
             # Stream results
             im0 = annotator.result()
@@ -325,25 +260,18 @@ def run(
                     save_path = str(Path(save_path).with_suffix('.mp4'))  # force *.mp4 suffix on results videos
                     vid_writer[i] = cv2.VideoWriter(save_path, cv2.VideoWriter_fourcc(*'mp4v'), fps, (w, h))
                 vid_writer[i].write(im0)
+
             prev_frames[i] = curr_frames[i]
-        
-        
 
     # Print results
-    # print("seen:",seen)
-    if seen:
-        t = tuple(x / seen * 1E3 for x in dt)  # speeds per image
-        LOGGER.info(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS, %.1fms strong sort update per image at shape {(1, 3, *imgsz)}' % t)
+    t = tuple(x / seen * 1E3 for x in dt)  # speeds per image
+    LOGGER.info(f'Speed: %.1fms pre-process, %.1fms inference, %.1fms NMS, %.1fms strong sort update per image at shape {(1, 3, *imgsz)}' % t)
     if save_txt or save_vid:
         s = f"\n{len(list(save_dir.glob('tracks/*.txt')))} tracks saved to {save_dir / 'tracks'}" if save_txt else ''
         LOGGER.info(f"Results saved to {colorstr('bold', save_dir)}{s}")
     if update:
         strip_optimizer(yolo_weights)  # update model (to fix SourceChangeWarning)
 
-
-
-def setTimeout(cbname,delay,*argments):
-    threading.Timer(delay,cbname,argments).start()
 
 def parse_opt():
     parser = argparse.ArgumentParser()
@@ -377,7 +305,6 @@ def parse_opt():
     parser.add_argument('--hide-class', default=False, action='store_true', help='hide IDs')
     parser.add_argument('--half', action='store_true', help='use FP16 half-precision inference')
     parser.add_argument('--dnn', action='store_true', help='use OpenCV DNN for ONNX inference')
-    parser.add_argument('--capture_device',default=0, type=int, help='USB capture_device')
     opt = parser.parse_args()
     opt.imgsz *= 2 if len(opt.imgsz) == 1 else 1  # expand
     print_args(vars(opt))
