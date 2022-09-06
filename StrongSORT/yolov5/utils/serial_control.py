a15b609f5a3aaf0fbaa950d49191e0aaa0e962dd
Carlos  chen <ccjjww1222@gmail.com>
2022-09-06 18:02:11 +0800

    Update serial_control.py

--- StrongSORT/yolov5/utils/serial_control.py
+++ StrongSORT/yolov5/utils/serial_control.py
@@ -69,11 +69,11 @@ class serial_control():
                         ret_all += str(response,"UTF-8")
                         response_arr = ret_all.splitlines()
                         ret = response_arr[len(response_arr)-1] if len(response_arr) > 0 else ""
-                        # self.logger.info("1--cnt:%s,send_cmd:uuid:%s,cmd:%s,ret:%s,difftime:%s,response:%s",cnt, uuid, cmd, ret, diff,ret_all)
+                        self.logger.info("1--cnt:%s,send_cmd:uuid:%s,cmd:%s,ret:%s,difftime:%s,response:%s",cnt, uuid, cmd, ret, diff,ret_all)
                         # time.sleep(0.1)
 
                         if(str(ret)=="0"): 
-                            # self.logger.info("send_cmd:uuid:%s,cmd:%s,ret:%s,difftime:%s,response:%s", uuid, cmd, ret, diff,ret_all)
+                            self.logger.info("send_cmd:uuid:%s,cmd:%s,ret:%s,difftime:%s,response:%s", uuid, cmd, ret, diff,ret_all)
                             ret_dict = {
                                 "uuid":uuid,
                                 "retsult":ret
