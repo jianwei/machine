1b1bd951dc93f979efd8d58db366f39de16e73b6
Carlos  chen <ccjjww1222@gmail.com>
2022-09-07 15:07:58 +0800

    Update detect.py

--- StrongSORT/yolov5/detect.py
+++ StrongSORT/yolov5/detect.py
@@ -238,6 +238,8 @@ def run(
                                 redis.set(done_key,time.time(),working_time_out)
                                 redis.set("is_working",1,working_time_out)
                                 setTimeout(work_obj.wheel,0.00001,"15")
+                            else:
+                                print("centery is outer:",centery)
                     else:
                         print("done_key:",done_key,"is done,is_working:",is_working)
                 else:
