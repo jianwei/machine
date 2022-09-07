5202d679f82bbfcafe61c7700e04ae64ab630cb8
Carlos  chen <ccjjww1222@gmail.com>
2022-09-07 14:13:26 +0800

    Update track.py

--- StrongSORT/track.py
+++ StrongSORT/track.py
@@ -270,6 +270,7 @@ def run(
                             # print("done_key:",done_key,"is_done2:",is_done)
                             centery = done["centery"]
                             # if(centery>1 )
+                            print("centery------------------------------------:",centery)
                             redis.set(done_key,1,working_time_out)
                             redis.set("is_working",1,working_time_out)
                             setTimeout(work_obj.wheel,0.00001,"15")
