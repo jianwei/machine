5def46d96a20bd55cec7476cb9df89f36c41771b
Carlos  chen <ccjjww1222@gmail.com>
2022-10-19 18:08:30 +0800

    Update yolov5.py

--- yolov5_mutiple_camera/yolov5.py
+++ yolov5_mutiple_camera/yolov5.py
@@ -74,7 +74,7 @@ class Darknet(object):
                         if view_img:
                             self.plot_one_box(xyxy, im0, color=(255, 0, 0), label=label)
                 
-                fps = 1/(t2-t1)
+                fps = round(1/(t2-t1),3)
                 cv2.putText(im0,"fps:{}".format(fps), (0, 30),0,1,(0,0,255),thickness=2,lineType=cv2.LINE_AA)
                 print(f'{s}Done. ({t2 - t1:.3f}s),fps:{1/(t2-t1)}')
 
